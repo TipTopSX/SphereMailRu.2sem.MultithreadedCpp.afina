0864974286eaf52601ed2d940205cbbdfb1ab208
Alexey Navolotsky <alexey@navolotsky.com>
2020-10-27 20:07:53 +0300

    hw5: single-thread epoll

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -2,8 +2,14 @@
 #define AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
 
 #include <cstring>
-
+#include <memory>
 #include <sys/epoll.h>
+
+#include "protocol/Parser.h"
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+#include <deque>
+#include <spdlog/logger.h>
 
 namespace Afina {
 namespace Network {
@@ -11,12 +17,13 @@ namespace STnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<spdlog::logger> &logger_, std::shared_ptr<Afina::Storage> &pStorage_)
+        : _socket(s), _logger{logger_}, _pStorage{pStorage_} {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return _running; }
 
     void Start();
 
@@ -31,6 +38,18 @@ private:
 
     int _socket;
     struct epoll_event _event;
+    bool _running{true};
+
+    std::shared_ptr<spdlog::logger> _logger;
+    std::shared_ptr<Afina::Storage> _pStorage;
+
+    std::size_t _arg_remains;
+    Protocol::Parser _parser;
+    std::string _argument_for_command;
+    std::unique_ptr<Execute::Command> _command_to_execute;
+    char _client_buffer[4096];
+    std::deque<std::string> _output;
+    bool _eof{false};
 };
 
 } // namespace STnonblock
