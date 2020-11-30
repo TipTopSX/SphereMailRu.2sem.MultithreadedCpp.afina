07959b72b63b4074071e4caf3fa5605d72bc9a0f
Alexey Navolotsky <alexey@navolotsky.com>
2020-11-30 13:13:25 +0300

    hw6 bugfix

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -2,36 +2,36 @@
 #define AFINA_NETWORK_MT_NONBLOCKING_CONNECTION_H
 
 #include <cstring>
+#include <sys/epoll.h>
 
 #include "../st_nonblocking/Connection.h"
-#include <sys/epoll.h>
 
 namespace Afina {
 namespace Network {
 namespace MTnonblock {
-typedef Afina::Network::STnonblock::Connection super;
 
-class Connection : public super {
+class Connection : public Afina::Network::STnonblock::Connection {
 public:
     Connection(int s, std::shared_ptr<spdlog::logger> &logger_, std::shared_ptr<Afina::Storage> &pStorage_)
-        : super(s, logger_, pStorage_) {
+        : Afina::Network::STnonblock::Connection(s, logger_, pStorage_) {
         _event.data.ptr = this;
     }
 
     void Start() override {
-        super::Start();
+        std::unique_lock<std::mutex> lock(_mutex);
+        Afina::Network::STnonblock::Connection::Start();
         _event.events |= EPOLLET;
     };
 
 protected:
     void DoRead() override {
         std::unique_lock<std::mutex> lock(_mutex);
-        super::DoRead();
+        Afina::Network::STnonblock::Connection::DoRead();
     };
 
     void DoWrite() override {
         std::unique_lock<std::mutex> lock(_mutex);
-        super::DoWrite();
+        Afina::Network::STnonblock::Connection::DoWrite();
     };
 
 private:
